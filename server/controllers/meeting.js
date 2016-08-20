6b5cf98f896b67a8185cdafccf8583b6e748096d
Brendan Metcalfe <bgmetcalfe@gmail.com>
2016-08-20 15:47:48 -0400

    basic meeting page works

--- server/controllers/meeting.js
+++ server/controllers/meeting.js
@@ -8,13 +8,15 @@ exports.create = function(req, res, next) {
   });
   meeting.save(function(err) {
     if(err) console.log('error creating a meeting', err);
-    res.send(meeting);
+    //res.send(meeting);
+    res.render('account/meeting');
   });
 }
 
 exports.get = function(req,res, next) {
   Meeting.find({}, function(err, meetings) {
-    res.send(meetings);
+    // res.send(meetings);
+    res.render('account/meeting');
   })
   .populate('attendees')
   .exec(function (err, meeting) {
@@ -31,64 +33,7 @@ exports.joinMeeting = function(req, res, next) {
   Meeting.findOneAndUpdate(req.params.id,
     {$push: { "attendees": req.body.attendee_id}},
     {safe: true, new: true}, function(err, meeting) {
-    res.send(meeting);
+    // res.send(meeting);
+    res.render('account/meeting');
   });
 }
-/**
- * POST /signup
- */
-// exports.signupPost = function(req, res, next) {
-//   req.assert('name', 'Name cannot be blank').notEmpty();
-//   req.assert('email', 'Email is not valid').isEmail();
-//   req.assert('email', 'Email cannot be blank').notEmpty();
-//   req.assert('password', 'Password must be at least 4 characters long').len(4);
-//   req.sanitize('email').normalizeEmail({ remove_dots: false });
-//
-//   var errors = req.validationErrors();
-//
-//   if (errors) {
-//     req.flash('error', errors);
-//     return res.redirect('/signup');
-//   }
-//
-//   User.findOne({ email: req.body.email }, function(err, user) {
-//     if (user) {
-//       req.flash('error', { msg: 'The email address you have entered is already associated with another account.' });
-//       return res.redirect('/signup');
-//     }
-//     user = new User({
-//       name: req.body.name,
-//       email: req.body.email,
-//       password: req.body.password
-//     });
-//     user.save(function(err) {
-//       req.logIn(user, function(err) {
-//         res.redirect('/testmeeting');
-//       });
-//     });
-//   });
-// };
-//
-//
-//   User.findById(req.user.id, function(err, user) {
-//     if ('password' in req.body) {
-//       user.password = req.body.password;
-//     } else {
-//       user.email = req.body.email;
-//       user.name = req.body.name;
-//       user.gender = req.body.gender;
-//       user.location = req.body.location;
-//       user.website = req.body.website;
-//     }
-//     user.save(function(err) {
-//       if ('password' in req.body) {
-//         req.flash('success', { msg: 'Your password has been changed.' });
-//       } else if (err && err.code === 11000) {
-//         req.flash('error', { msg: 'The email address you have entered is already associated with another account.' });
-//       } else {
-//         req.flash('success', { msg: 'Your profile information has been updated.' });
-//       }
-//       res.redirect('/account');
-//     });
-//   });
-// };
